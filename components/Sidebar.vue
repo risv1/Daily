01ec2b1b33b6245ffb81c800150b456e194706e3
risv1 <rishi.aangarai@gmail.com>
2024-05-09 08:10:32 +0530

    feat: add dash layout

---
+++ components/Sidebar.vue
@@ -0,0 +1,47 @@
+<template>
+  <div
+    class="xl:w-[7vw] lg:w-[7vw] md:w-[7vw] sm:w-[7vw] lg:flex md:flex sm:flex border h-[95vh] rounded-lg ml-3 bg-cyan-600 hidden flex-col justify-center items-center"
+  >
+    <div class="flex flex-col gap-10">
+      <Icon
+        @click="navigateTo(route.path)"
+        v-for="route in routes"
+        :name="route.icon"
+        :color="route.color"
+        :size="route.size"
+        class="p-2 rounded-full hover:bg-cyan-500 hover:cursor-pointer duration-200 ease-in-out"
+      />
+    </div>
+  </div>
+  <div class="lg:hidden md:hidden sm:hidden flex items-center justify-center absolute bottom-0 w-full h-[10vh] border bg-cyan-600">
+    <div class="flex flex-row gap-10">
+        <Icon
+            @click="navigateTo(route.path)"
+            v-for="route in routes"
+            :name="route.icon"
+            :color="route.color"
+            :size="route.size"
+            class="p-2 rounded-full hover:bg-cyan-500 hover:cursor-pointer duration-200 ease-in-out"
+        />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const routes = [
+  {
+    icon: "material-symbols:home-and-garden-outline",
+    color: "white",
+    size: 50,
+    path: "/me",
+  },
+  { icon: "iconoir:task-list", color: "white", size: 50, path: "/me/tasks" },
+  {icon: "material-symbols:exercise-outline-sharp", color: "white", size: 50, path: "/me/health"},
+  {
+    icon: "material-symbols:account-circle-full",
+    color: "white",
+    size: 50,
+    path: "/profile",
+  },
+];
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,45 +1,63 @@
 <template>
-  <div
-    class="w-screen h-screen flex flex-row items-center bg-indigo-950 bg-opacity-60"
-  >
+  <LoadWrapper>
     <div
-      class="fixed inset-0 opacity-40 bg-indigo-950 -z-10"
-      style="
-        background-image: url('/Dazai.png');
-        background-size: cover;
-        background-position: center;
-      "
-    ></div>
-    <div
-      class="lg:w-3/5 lg:h-4/5 lg:p-10 lg:justify-center md:w-3/5 md:h-4/5 md:p-10 md:justify-center px-5 w-full h-full flex gap-7 flex-col justify-center items-center"
+      class="w-screen h-screen flex flex-row items-center bg-indigo-950 bg-opacity-60"
     >
-      <h1 class="lg:text-6xl text-4xl text-center text-white font-semibold">
-        Welcome to <span class="text-cyan-400 font-medium">Daily</span>.
-      </h1>
-      <p
-        class="w-2/3 text-center xl:block lg:block md:block sm:block text-white lg:text-3xl text-xl font-normal"
+      <div
+        class="fixed inset-0 opacity-40 -z-10"
+        style="
+          background-image: url('/Dazai.png');
+          background-size: cover;
+          background-position: center;
+        "
+      ></div>
+      <div
+        class="lg:w-3/5 lg:h-4/5 lg:p-10 lg:justify-center md:w-3/5 md:h-4/5 md:p-10 md:justify-center px-5 w-full h-full flex gap-7 flex-col justify-center items-center"
       >
-        An application to help me navigate around my frequent visits and manage
-        my everyday schedule.
-      </p>
-      <div class="flex flex-row gap-5">
-        <Button
-          @click="() => navigateTo('/me')"
-          class="lg:block md:block sm:block bg-cyan-600 hover:bg-cyan-700"
-          >Get started</Button
+        <h1 class="xl:text-7xl lg:text-6xl text-4xl text-center text-white font-semibold">
+          Welcome to <span class="text-cyan-400 font-medium">Daily</span>.
+        </h1>
+        <p
+          class="w-2/3 text-center xl:block lg:block md:block sm:block text-white xl:text-4xl lg:text-3xl text-xl font-normal"
         >
-        <Button
-          @click="() => navigateTo('/login')"
-          class="lg:block md:block sm:block bg-cyan-600 hover:bg-cyan-700 hidden"
-          >Go to Login</Button
-        >
+          An application to help me navigate around my frequent visits and
+          manage my everyday schedule.
+        </p>
+        <div class="flex justify-center gap-5 w-full">
+          <Button
+            @click="() => navigateTo('/me')"
+            class="lg:block md:block sm:block bg-cyan-600 w-2/5 hover:bg-cyan-700 xl:text-xl xl:h-14"
+            >Get started</Button
+          >
+        </div>
+      </div>
+      <div
+        class="lg:w-2/5 lg:flex md:flex md:w-2/5 xl:text-5xl lg:text-3xl md:text-2xl text-md h-full flex-col justify-center hidden"
+      >
+        <p class="text-white italic w-4/5">
+          "{{ chosenQuote.quote }}"
+        </p>
+        <p class="text-white font-semibold pl-20 pt-5">
+          - {{ chosenQuote.by }}
+        </p>
       </div>
     </div>
-  </div>
+  </LoadWrapper>
 </template>
 
 <script setup>
 import { useAuthStore } from "@/store/auth";
+import { quotes } from "@/utils/quotes";
 
 const { user } = useAuthStore();
+const chosenQuote = ref("");
+
+const chooseRandomQuote = () => {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  chosenQuote.value = quotes[randomIndex];
+};
+
+onMounted(() => {
+  chooseRandomQuote();
+});
 </script>
